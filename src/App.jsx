1149bebcc51f8dd88091c40d78bc0e9191aa8280
matan1542 <matan8380@gmail.com>
2022-02-16 12:09:20 +0200

    deployment attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import logo from './logo.svg';
 import './style/style.scss';
-import axios from 'axios'
-import { sentinelService } from './services/sentinel.service.js'
-import { useEffect, useMemo, useState } from 'react';
 import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
+  RecoilRoot
 } from 'recoil';
 import Home from './pages/Home';
 import Header from './components/Header';

4aed1584f46f570f04f3d9bf11519a477a9c2a38
matan1542 <matan8380@gmail.com>
2022-02-17 08:33:43 +0200

    date fix

--- src/services/date.service.js
+++ src/services/date.service.js
@@ -3,8 +3,8 @@ function getRandomDates() {
     let date = new Date()
     let currYear = date.getFullYear()
     const firstDate = new Date(utilsService.getRandomInt(2017, currYear - 1), utilsService.getRandomInt(0, 5), 1)
-    const secondDate = new Date(firstDate.getFullYear(), firstDate.getMonth() + 12, 1)
-    return [firstDate.toISOString(), secondDate.toISOString()]
+    const secondDate = new Date(firstDate.getFullYear(), firstDate.getMonth() + 1, 1).setMonth(firstDate.getMonth() + 12)
+    return [firstDate.toISOString(), new Date(secondDate.toISOString()).toISOString]
 
 }
 
